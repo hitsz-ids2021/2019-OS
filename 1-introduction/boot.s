ed05b4cf8c7dbb3ba37b1018bf2c864db815d664
david <liucy@yunanbao.com.cn>
2019-10-08 07:42:50 +0800

    Update boot.s
    modify message

--- 1-introduction/boot.s
+++ 1-introduction/boot.s
@@ -1,4 +1,6 @@
 .code16
+.text
+
 init:
     mov %cs, %ax
     mov %ax, %ds
@@ -9,13 +11,13 @@ init:
 DispStr:
     mov $BootMessage, %ax
     mov %ax, %bp
-    mov $16, %cx
+    mov $22, %cx
     mov $0x1301, %ax
     mov $0x000c, %bx
     mov $0, %dl
     int $0x10
     ret
 
-BootMessage:    .asciz "Hello, OS world!"
+BootMessage:    .asciz "Hello, HITSZ OS World!"
 .fill 510-(.-init), 1, 0
 .word 0xaa55
