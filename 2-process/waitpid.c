adf89605f21978de63f0f3a8d62088d30b56be12
OliviaZL <zhangliuzhl@163.com>
2019-10-14 17:30:05 +0800

    update code

--- 2-process/waitpid.c
+++ 2-process/waitpid.c
@@ -10,12 +10,13 @@ void fork8() {
     pid_t pid[N];
     int i, child_status;
 
+
     for (i = 0; i < N; i++)
-        if ((pid[i] = fork()) == 0) {
+        if ((pid[i] = fork()) == 0)
             exit(100+i); /* Child */
-        }
-    for (i = 0; i < N; i++) { /* Parent */
-        pid_t wpid = wait(&child_status);
+	    
+    for (i = N-1; i >= 0; i--) {
+        pid_t wpid = waitpid(pid[i], &child_status, 0);
         if (WIFEXITED(child_status))
             printf("Child %d terminated with exit status %d\n",
                    wpid, WEXITSTATUS(child_status));
