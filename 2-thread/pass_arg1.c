203e7d2bb1b32366e7961faadcbd3922c9c8627c
lps2017H <593284164@qq.com>
2019-10-18 15:32:53 +0800

    Modify pass_arg1.c

--- 2-thread/pass_arg1.c
+++ 2-thread/pass_arg1.c
@@ -12,26 +12,34 @@ void *PrintHello(void *threadid)
 
    sleep(1);
    taskid = (long) threadid;
-   printf("Hello from thread %ld\n", taskid);
+   printf("Thread %d: %s\n", taskid, messages[taskid]);
    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-  pthread_t threads[NUM_THREADS];
-  long taskids[NUM_THREADS];
-  int rc, t;
+pthread_t threads[NUM_THREADS];
+long taskids[NUM_THREADS];
+int rc, t;
 
-  for(t=0;t<NUM_THREADS;t++) {
-    taskids[t] = t;
-    printf("Creating thread %d\n", t);
-    rc = pthread_create(&threads[t], NULL, PrintHello, (void *) taskids[t]);
-    if (rc) {
-      printf("ERROR; return code from pthread_create() is %d\n", rc);
-      exit(-1);
+messages[0] = "English: Hello World!";
+messages[1] = "French: Bonjour, le monde!";
+messages[2] = "Spanish: Hola al mundo";
+messages[3] = "Klingon: Nuq neH!";
+messages[4] = "German: Guten Tag, Welt!"; 
+messages[5] = "Russian: Zdravstvuyte, mir!";
+messages[6] = "Japan: Sekai e konnichiwa!";
+messages[7] = "Latin: Orbis, te saluto!";
+
+for(t=0;t<NUM_THREADS;t++) {
+  taskids[t] = t;
+  printf("Creating thread %d\n", t);
+  rc = pthread_create(&threads[t], NULL, PrintHello, (void *) taskids[t]);
+  if (rc) {
+    printf("ERROR; return code from pthread_create() is %d\n", rc);
+    exit(-1);
     }
   }
-  pthread_exit(NULL);
-}  
 
-
+pthread_exit(NULL);
+}
